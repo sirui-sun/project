c72f2f3dd4f0d9cf91a318b213c1e967772c564b
Sirui Sun <sirui.sun1@gmail.com>
2018-03-01 22:37:26 -0800

    add time buffer

--- 2/PlayerAI_3.py
+++ 2/PlayerAI_3.py
@@ -1,9 +1,11 @@
 from random import randint
 from BaseAI_3 import BaseAI
+import time
 
 NEG_INF = float("-inf")
 POS_INF = float("inf")
 SPAWNED_TILES_VALUES = [2,4]
+TIME_BUFFER = 0.05 # number of seconds remaining at which we end the depth-first traversal
 
 class PlayerAI(BaseAI):
 
@@ -11,12 +13,19 @@ class PlayerAI(BaseAI):
 	def isTerminalState(self, grid):
 		return len(grid.getAvailableMoves()) == 0 
 
+	# are we in the buffer time?
+	def inBufferTime(self, start_time):
+		time_elapsed = time.clock() - start_time
+		time_remaining = 0.25 - time_elapsed
+		return time_remaining < TIME_BUFFER
+
 	def generateHeuristic(self, grid):
 		return (None, len(grid.getAvailableCells()))
 
 	# maximize - try and get to 2048 by moving up, left, down, right
 	# returns: (move, utility)
-	def maximize(self, grid, alpha, beta, depth):
+	def maximize(self, grid, alpha, beta, depth, time_start):
+
 		if self.isTerminalState(grid) or depth == 4:
 			return self.generateHeuristic(grid)
 
@@ -25,12 +34,12 @@ class PlayerAI(BaseAI):
 		for move in grid.getAvailableMoves():
 			newGrid = grid.clone()
 			newGrid.move(move)
-			(_, currentUtility) = self.minimize(newGrid, alpha, beta, depth+1)
+			(_, currentUtility) = self.minimize(newGrid, alpha, beta, depth+1, time_start)
 
 			if (currentUtility > maxUtility):
 				(maxMove, maxUtility) = (move, currentUtility)
 
-			if (maxUtility >= beta):
+			if (maxUtility >= beta or self.inBufferTime(time_start)):
 				break
 
 			if (maxUtility > alpha):
@@ -40,7 +49,7 @@ class PlayerAI(BaseAI):
 
 	# minimize - try to minimize board score by placing new tiles
 	# returns: (move, utility)
-	def minimize(self, grid, alpha, beta, depth):
+	def minimize(self, grid, alpha, beta, depth, time_start):
 
 		if self.isTerminalState(grid) or depth == 4:
 			return self.generateHeuristic(grid)
@@ -51,12 +60,12 @@ class PlayerAI(BaseAI):
 			for value in SPAWNED_TILES_VALUES:
 				newGrid = grid.clone()
 				newGrid.insertTile(cell, value)
-				(_, currentUtility) = self.maximize(newGrid, alpha, beta, depth+1)
+				(_, currentUtility) = self.maximize(newGrid, alpha, beta, depth+1, time_start)
 
 				if (currentUtility < minUtility):
 					minUtility = currentUtility
 
-				if (minUtility <= alpha):
+				if (minUtility <= alpha or self.inBufferTime(time_start)):
 					break
 
 				if (minUtility < beta):
@@ -65,7 +74,9 @@ class PlayerAI(BaseAI):
 		return (None, minUtility)
 	
 	def getMove(self, grid):
-		(move, utility) = self.maximize(grid, NEG_INF, POS_INF, 0)
+		time_start = time.clock()
+		(move, utility) = self.maximize(grid, NEG_INF, POS_INF, 0, time_start)
+		time_elapsed = time.clock() - time_start
 		return move
 
 		# moves = grid.getAvailableMoves()
